bcb609a8e248123ae96a75345af87c4bfa4611d0
SRI-VARDHAN-ADIKI <2300033389@kluniversity.in>
2025-08-25 12:40:45 +0530

    additional commit

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -10,35 +10,13 @@
         <br/>
         <br/>
         <br/>
-<<<<<<< HEAD
-        <a href="logout" style={styles.link}>Logout..</a>
-=======
+
+
         <a href="logout" className="link">Logout</a>
->>>>>>> a19c836 (added navbar.css file)
+
 
         </div>
     );
 
 };
 
-<<<<<<< HEAD
-const styles = {
-  navbar: {
-    height: "100vh",
-    width: "250px",
-    backgroundColor: "#f1f1f1",
-    display: "flex",
-    flexDirection: "column",
-    paddingTop: "20px",
-    borderRight: "2px solid #3333",   
-  },
-  link: {
-    padding: "12px 16px",
-    textDecoration: "none",
-    color: "black",
-    display: "block",
-  }
-};
-=======
-
->>>>>>> a19c836 (added navbar.css file)
