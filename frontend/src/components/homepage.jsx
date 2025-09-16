2d35643a1992076c14cdc37b76b6c0795f19fbb6
SRI VARDHAN ADIKI <162698242+SRI-VARDHAN-ADIKI@users.noreply.github.com>
2025-09-16 23:08:17 +0530

    Update homepage.jsx

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -6,6 +6,7 @@ export default function HomePage() {
         <div className="home-page">
             <h1>Welcome to the Expense Tracker</h1>
             <p>Track your expenses, manage your budget, and achieve your financial goals.</p>
+            <p>This project is under construction</p>
         </div>
     );
 };
